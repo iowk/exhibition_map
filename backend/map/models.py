40b392543df41138a0d069f8f497fe24da6f2803
iowk <gj3bp6123@gmail.com>
2022-09-17 07:21:58 +0800

    Add password field to Customuser

--- backend/map/models.py
+++ backend/map/models.py
@@ -10,6 +10,9 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
+    password = models.CharField(
+        max_length=20,
+    )
 
 # Create your models here.
 class Image(models.Model): #Landmark and content images
