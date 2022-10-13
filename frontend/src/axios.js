440870f21c341fce6f689c7beb6812c9354ead51
iowk <gj3bp6123@gmail.com>
2022-10-13 09:53:14 +0800

    Add frontend settings

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,10 +1,11 @@
 import _axios from "axios"
+import { backendPath } from "./settings";
 
 const axios = (token) => {
     if(token) _axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
     else delete _axios.defaults.headers.common["Authorization"];
     return _axios.create({
-        baseURL: 'http://localhost:8000',
+        baseURL: backendPath,
         timeout: 10000,
     })
 }
