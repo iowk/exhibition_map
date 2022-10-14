c562e6e80681a97a825b42ef38cefb5d2d48f5f7
iowk <gj3bp6123@gmail.com>
2022-10-14 10:23:09 +0800

    Delete media url

--- backend/google_map_test/urls.py
+++ backend/google_map_test/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('map/', include('map.urls')),
     path('api-auth/', include('rest_framework.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
