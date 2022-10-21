bca8955113f9c73b7109cd9c1fc9fab7f6c86ccf
iowk <gj3bp6123@gmail.com>
2022-10-21 21:14:54 +0200

    Change charfield lengths

--- backend/map/models.py
+++ backend/map/models.py
@@ -14,7 +14,7 @@ class CustomUser(AbstractUser):
 
 class Image(models.Model): # Landmark and content images
     created = models.DateTimeField(editable=False)
-    name = models.CharField(max_length=100, blank=True)
+    name = models.CharField(max_length=1000, blank=True)
     src = models.ImageField(upload_to='images/', default='images/empty.jpg')
     class Meta:
         abstract = True
@@ -40,11 +40,11 @@ class Comment(models.Model): # Landmark and content comments
 
 class Landmark(models.Model):
     owner = models.ForeignKey(CustomUser, related_name='landmark', on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=1000)
     lat = models.FloatField(default = 0.0)
     lng = models.FloatField(default = 0.0)
     zIndex = models.IntegerField(default = 1)
-    link = models.CharField(max_length=200, blank=True)
+    link = models.CharField(max_length=1000, blank=True)
     coverImageSrc = models.ImageField(upload_to='images/', default='images/empty.jpg')
     is_visible = models.BooleanField(default=False)
     def __str__(self):
@@ -72,10 +72,10 @@ class LandmarkComment(Comment):
 class Content(models.Model):
     owner = models.ForeignKey(CustomUser, related_name='content', on_delete=models.SET_NULL, null=True)
     landmark = models.ForeignKey(Landmark, related_name='contents', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=1000)
     startDate = models.DateField(blank=True)
     endDate = models.DateField(blank=True)
-    link = models.CharField(max_length=200, blank=True)
+    link = models.CharField(max_length=1000, blank=True)
     description = models.CharField(max_length=3000, blank=True)
     coverImageSrc = models.ImageField(upload_to='images/', default='images/empty.jpg')
     is_visible = models.BooleanField(default=False)
